312928288e4101a8b508b1270c5a0ff3c9f30db5
sixone <931039665@qq.com>
2018-11-16 20:27:56 +0800

    信息更新

---
+++ index.js
@@ -0,0 +1,53 @@
+module.exports = {
+	// 模版标识
+	name: 'house-two',
+	// 模版引擎
+	engine: 'pug',
+
+	// 页面
+	pages: [
+		{
+			name: '首页',
+			// 路径
+			path: '/',
+			// 预览图
+			thumbnail: 'home.png',
+		},
+		{
+			name: '关于我们',
+			// 路径
+			path: 'about',
+			// 预览图
+			thumbnail: 'about.png',
+		},
+		{
+			name: '服务',
+			// 路径
+			path: 'services',
+			// 预览图
+			thumbnail: 'services.png',
+		},
+		{
+			name: '产品',
+			// 路径
+			path: 'projects',
+			// 预览图
+			thumbnail: 'projects.png',
+		},
+		{
+			name: '定价',
+			// 路径
+			path: 'pricing',
+			// 预览图
+			thumbnail: 'pricing.png',
+		},
+		{
+			name: '联系我们',
+			// 路径
+			path: 'contact',
+			// 预览图
+			thumbnail: 'contact.png',
+		},
+
+	]
+}
